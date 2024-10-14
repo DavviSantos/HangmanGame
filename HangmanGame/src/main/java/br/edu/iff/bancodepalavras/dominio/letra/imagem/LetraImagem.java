0f1402233946ad6cceb165305056d43bcdb81702
davvisantos <davvisantos25@gmail.com>
2024-10-14 12:51:45 -0300

    feat(letra): adição das classes LetraTexto e LetraImagem

---
+++ HangmanGame/src/main/java/br/edu/iff/bancodepalavras/dominio/letra/imagem/LetraImagem.java
@@ -0,0 +1,16 @@
+package main.java.br.edu.iff.bancodepalavras.dominio.letra.imagem;
+
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.Letra;
+
+public class LetraImagem extends Letra {
+    
+    public LetraImagem(char codigo) {
+        super(codigo);
+    }
+
+    @Override
+    public void exibir(Object contexto) {
+        // Implementar exibir() com lógica de exibição de imagem
+        // não precisa implementar exibir(), basta deixar os métodos vazios
+    }
+}
