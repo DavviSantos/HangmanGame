2411449619e48441106ac3d47ebdf15d920f8ab8
L-Bernardes <lorran.bernardes@gsuite.iff.edu.br>
2024-10-26 11:37:28 -0300

    Métodos criados

--- HangmanGame/src/main/java/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
+++ HangmanGame/src/main/java/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
@@ -1,5 +1,65 @@
 package main.java.br.edu.iff.bancodepalavras.dominio.palavra;
 
-public class Palavra {
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.LetraFactory;
+import main.java.br.edu.iff.dominio.ObjetoDominioImpl;
+
+public class Palavra extends ObjetoDominioImpl {
     
+    public void setLetraFactory(LetraFactory factory ){
+
+    }
+
+    public LetraFactory getLetraFactory(){
+        return factory;
+    }
+    
+    public Palavra criar(long id, String palavra, Tema tema){
+
+    }
+    
+    public Palavra reconstruir(long id, String palavra, Tema tema){
+
+    }
+
+    public Palavra (long id, String palavra, Tema tema){
+
+    }
+
+    public Letra[] getLetras(){
+
+    }
+
+    public Letra getLetra(int posicao){
+
+    }
+
+    public void exibir(Object contexto){
+
+    }
+
+    public void exibir(Object contexto, boolean[] posicoes){
+
+    }
+
+    public int[] tentar(char codigo){
+
+    }
+
+    public Tema getTema(){
+
+    }
+
+    public boolean comparar(String palavra){
+
+    }
+
+    public int getTamanho(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Palavra []";
+    }
 }
