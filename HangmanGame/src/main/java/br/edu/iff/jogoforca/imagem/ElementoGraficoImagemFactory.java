7ab75bf564e1ef61e4155aa44f970030416d71c2
davvisantos <davvisantos25@gmail.com>
2024-10-27 17:05:55 -0300

    feat: Criação de ElementoGraficoTextoFactory e ElementoGraficoImagemFactory

--- HangmanGame/src/main/java/br/edu/iff/jogoforca/imagem/ElementoGraficoImagemFactory.java
+++ HangmanGame/src/main/java/br/edu/iff/jogoforca/imagem/ElementoGraficoImagemFactory.java
@@ -1,5 +1,34 @@
 package main.java.br.edu.iff.jogoforca.imagem;
 
-public class ElementoGraficoImagemFactory {
-    
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import main.java.br.edu.iff.jogoforca.ElementoGraficoFactory;
+import main.java.br.edu.iff.jogoforca.dominio.boneco.Boneco;
+import main.java.br.edu.iff.jogoforca.dominio.boneco.imagem.BonecoImagemFactory;
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.imagem.LetraImagemFactory;
+
+public class ElementoGraficoImagemFactory implements ElementoGraficoFactory {
+
+    private final LetraImagemFactory letraImagemFactory;
+    private final BonecoImagemFactory bonecoImagemFactory;
+
+    // Construtor
+    public ElementoGraficoImagemFactory() {
+        this.letraImagemFactory = LetraImagemFactory.getSoleInstance(); // Acessa o singleton de LetraImagemFactory
+        this.bonecoImagemFactory = BonecoImagemFactory.getSoleInstance(); // Acessa o singleton de BonecoImagemFactory
+    }
+
+    @Override
+    public Boneco getBoneco() {
+        return bonecoImagemFactory.getBoneco(); // Retorna um boneco da fábrica
+    }
+
+    @Override
+    public Letra getLetra(char codigo) {
+        return letraImagemFactory.getLetra(codigo); // Retorna uma letra da fábrica
+    }
+
+    @Override
+    public Letra getLetraEncoberta() {
+        return letraImagemFactory.getLetraEncoberta(); // Retorna a letra encoberta da fábrica
+    }
 }
--- HangmanGame/src/main/java/br/edu/iff/jogoforca/texto/ElementoGraficoTextoFactory.java
+++ HangmanGame/src/main/java/br/edu/iff/jogoforca/texto/ElementoGraficoTextoFactory.java
@@ -1,7 +1,33 @@
 package main.java.br.edu.iff.jogoforca.texto;
 
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import main.java.br.edu.iff.bancodepalavras.dominio.letra.texto.LetraTextoFactory;
 import main.java.br.edu.iff.jogoforca.ElementoGraficoFactory;
+import main.java.br.edu.iff.jogoforca.dominio.boneco.Boneco;
+import main.java.br.edu.iff.jogoforca.dominio.boneco.texto.BonecoTextoFactory;
 
-public class ElementoGraficoTextoFactory implements ElementoGraficoFactory{
-    
+public class ElementoGraficoTextoFactory implements ElementoGraficoFactory {
+    private final LetraTextoFactory letraTextoFactory;
+    private final BonecoTextoFactory bonecoTextoFactory;
+
+    // Construtor
+    public ElementoGraficoTextoFactory() {
+        this.letraTextoFactory = LetraTextoFactory.getSoleInstance(); // Obter a instância única da fábrica de letras
+        this.bonecoTextoFactory = BonecoTextoFactory.getSoleInstance(); // Obter a instância única da fábrica de bonecos
+    }
+
+    @Override
+    public Boneco getBoneco() {
+        return bonecoTextoFactory.getBoneco(); // Retorna um boneco da fábrica de bonecos
+    }
+
+    @Override
+    public Letra getLetra(char codigo) {
+        return letraTextoFactory.getLetra(codigo); // Retorna uma letra da fábrica de letras
+    }
+
+    @Override
+    public Letra getLetraEncoberta() {
+        return letraTextoFactory.getLetraEncoberta(); // Retorna a letra encoberta da fábrica de letras
+    }
 }
