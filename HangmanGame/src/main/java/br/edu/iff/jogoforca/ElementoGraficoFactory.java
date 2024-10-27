66e2954ad58504d3ef1d870fed824a46ee273f07
davvisantos <davvisantos25@gmail.com>
2024-10-27 16:07:14 -0300

    feat: Criação de LetraFactoryImpl

--- HangmanGame/src/main/java/br/edu/iff/jogoforca/ElementoGraficoFactory.java
+++ HangmanGame/src/main/java/br/edu/iff/jogoforca/ElementoGraficoFactory.java
@@ -3,6 +3,6 @@ package main.java.br.edu.iff.jogoforca;
 import main.java.br.edu.iff.bancodepalavras.dominio.letra.LetraFactory;
 import main.java.br.edu.iff.jogoforca.dominio.boneco.BonecoFactory;
 
-public interface ElementoGraficoImagemFactory extends BonecoFactory, LetraFactory {
+public interface ElementoGraficoFactory extends BonecoFactory, LetraFactory {
     
 }
